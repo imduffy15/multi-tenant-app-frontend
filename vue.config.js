9e63265f904c95354862236c4b7b0abbf75001e0
Ian Duffy <ian@ianduffy.ie>
2019-01-28 23:09:21 +0000

    Use hostname for keycloak so cookies are picked up

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     disableHostCheck: true,
     proxy: {
       "^/auth": {
-        target: "http://localhost:9080",
+        target: "http://account.ianduffy.localdev:9080",
         secure: false,
         xfwd: false,
         changeOrigin: true
